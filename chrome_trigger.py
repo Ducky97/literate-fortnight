98ea94d6a440f5fc04bfc4e4881b30cfa2250938
Ducky97 <38096005+Ducky97@users.noreply.github.com>
2020-01-16 16:34:47 +0800

    Add files via upload

--- chrome_trigger.py
+++ chrome_trigger.py
@@ -3,77 +3,140 @@ import re
 import time
 import asyncio
 import multiprocessing
+import aiofiles
 from lxml import etree
 from selenium import webdriver
 from multiprocessing import Pool
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-urls = ["https://dagyy.v.zzu.edu.cn/login",
-     "https://mail.v.zzu.edu.cn",
-     "https://uts.zuel.edu.cn/Home/studentCommon/studentLogin.jsp",
-     "https://c.zju.edu.cn/webapps/login/",
-     "https://mail.zju.edu.cn",
-     "https://zlgc.ynu.edu.cn/zlgc/index.do",
-     "https://webmail.ynu.edu.cn/",
-     "http://ydarts.ybu.edu.cn/",
-     "https://mail.ybu.edu.cn",
-     "https://examination.xmu.edu.cn/jiaofei/login/",
-     "http://weixin.lib.xju.edu.cn",
-     "http://mail.xju.edu.cn"
-     ]
+
+
+# 防重的布隆函数
+from pybloom_live import ScalableBloomFilter as SBF
+global bf_ready, bf_file
+global option, school_name
+option = webdriver.ChromeOptions()
+option.add_experimental_option('excludeSwitches', ['enable-automation'])
+
+urls = [
+    "https://id.tsinghua.edu.cn/"
+]
+
+
+# async def wrt(url):
+#     global school_name, bf_file
+#     if url not in bf_file:
+#         bf_file.add(url)
+#         async with aiofiles.open(school_name + "_login_url.txt", 'a+') as afp:
+#             print("Login find! Url: {}".format(url))
+#             await afp.write(url)
+def wrt(url):
+    global school_name, bf_file
+    if url not in bf_file:
+        bf_file.add(url)
+        f_w = open(school_name + "_login_url.txt", 'a+')
+        f_w.write(url)
+        print("Login find! Url: {}".format(url))
+
+def Extractor(url):
+    # print(url)
+    num = url.count("/")
+    if "?" in url:
+        url_ = url.strip().split("?", 1)
+        return url_[0]
+    elif num >= 3:
+        url_ = url.strip().rsplit("/", 1)
+        return url_[0]
+    else:
+        return url.strip()
+
 
 def spider_sub_page(link):
-    print(link)
-    # time.sleep(5)
-    driver_s = webdriver.Chrome()
+    global option
+    driver_s = webdriver.Chrome(options=option)
+    # driver_s.set_script_timeout(10)
     driver_s.get(link)
-    WebDriverWait(driver_s, 10)
-    check(driver_s)
-    driver_s.close()
-    driver_s.quit()
+    try:
+        class ec(EC):
+            pass
+        WebDriverWait(driver_s, 10, ).until(
+            ec.presence_of_element_located((By.XPATH, "//input[@type='password']"))
+        )
+        # print("find!! {}".format(driver_s.current_url))
+        print('hello')
+        wrt(driver_s.current_url)
+    finally:
+        driver_s.close()
+        driver_s.quit()
 
-def get_sub_links(driver):
-    doc = driver.page_source
-    html = etree.HTML(doc)
-    # 查找所有的links
-    links = list()
-    for link in html.xpath("//a[@href]/@href"):
-        if ("javascript" in link) and ("void" in link):
-            continue
-        if not (re.match(r'http://', link) or re.match(r'https://', link)):
-            link = driver.current_url + link
-        links.append(link)
-    # print(links)
-    return links
 
-def check(driver):
-    doc = driver.page_source
-    html = etree.HTML(doc)
-    # 查找login
-    login = html.xpath("//input[@type='password']")
-    if login:
-        print("yes!")
-    else:
-        print("no!")
+def get_sub_links(driver, url):
+    try:
+        doc = driver.page_source
+        html = etree.HTML(doc)
+        # 查找所有的links
+        links = list()
+        for link in html.xpath("//a[@href]/@href"):
+            if ("javascript:" in link) or (".pdf" in link):
+                continue
+            if not (re.match(r'http://', link) or re.match(r'https://', link)):
+                link = url + link
+            links.append(link)
+        # print(links)
+        return links
+    except:
+        return
+
+# def check(driver):
+#     print("now, checking {}".format(driver.current_url))
+#     doc = driver.page_source
+#     html = etree.HTML(doc)
+#     # 查找login
+#     login = html.xpath("//input[@type='password']")
+#     if login:
+#         print("find!!, {}".format(driver.current_url))
+#     else:
+#         print("no!")
+
 
 async def spider_one_page(url):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    WebDriverWait(driver, 10)
-    check(driver)
+    global bf_ready, option, school_name
+    if not Extractor(url) in bf_ready:
+        # print("1"+Extractor(url))
+        bf_ready.add(Extractor(url))
+        driver = webdriver.Chrome(options=option)
+        print(url)
+        driver.get(url)
+        try:
+            class ec(EC):
+                pass
+            WebDriverWait(driver, 10, ignored_exceptions=True).until(
+                ec.presence_of_element_located((By.XPATH, "//input[@type='password']"))
+            )
+            # print("find!! {}".format(driver.current_url))
+            wrt(driver.current_url)
+        finally:
+            links = get_sub_links(driver, url)
+            ret_lis = list()
+            print(links)
+            pool = Pool()
+            for link in links:
+                if Extractor(link) in bf_ready:
+                    continue
+                if not school_name in link:
+                    continue
+                else:
+                   bf_ready.add(Extractor(link))
+                   ret = pool.apply_async(spider_sub_page, args=(link,))
+                   ret_lis.append(ret)
+            pool.close()
+            pool.join()
+            driver.close()
+            driver.quit()
 
-    # 打算在这里开始多进程'
-    links = get_sub_links(driver)
-    ret_lis = list()
-    pool = Pool()
-    for link in links:
-        ret = pool.apply_async(spider_sub_page, args=(link,))
-        ret_lis.append(ret)
-    pool.close()
-    pool.join()
 
-    driver.close()
-    driver.quit()
 
 
 
@@ -83,10 +146,24 @@ def spider():
     loop.run_until_complete(asyncio.gather(*cor))
 
 def main():
+    global bf_ready, bf_file
+    bf_ready = SBF(initial_capacity=len(urls)*50, error_rate=0.001, mode=SBF.LARGE_SET_GROWTH)
+    bf_file = SBF(initial_capacity=len(urls)*50, error_rate=0.001, mode=SBF.LARGE_SET_GROWTH)
     spider()
     return 0
 
 if __name__ == "__main__":
+
+    global school_name
+    school_name = "tsinghua"
+    # f_r = open(school_name + ".edu.cn.txt", 'r')
+    # line = f_r.readline()
+    # while line:
+    #     line = line.strip()
+    #     if len(line):
+    #         print(line)
+    #     line = f_r.readline()
+    #     urls.append(line)
     start_time = time.time()
     main()
     print('Use time:{:.2f}s'.format(time.time() - start_time))
